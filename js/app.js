45ff05104a3ba6a84de1f3a732eafe14b389cdb1
Måns Peterson <MP@server.manspeterson.se>
2017-04-25 14:28:11 +0200

    added dynamic stages

--- js/app.js
+++ js/app.js
@@ -8,6 +8,22 @@
 // }
 
 
+function getUrlParameter(sParam) {
+    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : sParameterName[1];
+        }
+    }
+    return false;
+};
+
 function authenticated(){
     if (Cookies.getJSON('session') != undefined) {
         return true;
@@ -337,21 +353,7 @@ function getPins(username, boardname) {
     });
 };
 
-var getUrlParameter = function getUrlParameter(sParam) {
-    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
-        sURLVariables = sPageURL.split('&'),
-        sParameterName,
-        i;
 
-    for (i = 0; i < sURLVariables.length; i++) {
-        sParameterName = sURLVariables[i].split('=');
-
-        if (sParameterName[0] === sParam) {
-            return sParameterName[1] === undefined ? true : sParameterName[1];
-        }
-    }
-    return false;
-};
 
 $('#boardname').on('keypress', function(e) {
     if (e.which === 13) {
